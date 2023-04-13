d05d61e5bd0121891173daee0c4368b7f77b36c9
Eugene-Myong <esmyong@ucsd.edu>
2023-04-13 10:07:28 -0700

    Update SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler{
+     // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Eugene's Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+
+    public String handleRequest(URI url){
+        ArrayList<String> words = new ArrayList<String>();
+        if(url.getPath().equals("/add"))
+        {
+            String[] parameters = url.getQuery().split("=");
+            if(parameters[0].equals("s"))
+            {
+                words.add(parameters[1]);
+            }
+        }
+        else if(url.getPath().equals("/search"))
+        {
+            String parameters[] = url.getQuery().split("=");
+            if(parameters[0].equals("s")){
+            String result = "";
+            {
+              for(int i = 0; i < words.size(); i++)
+              {
+                if(words.get(i).contains(parameters[1]))
+                {
+                   result += words.get(i) + " "; 
+                }
+              }
+            }
+            return result;
+         }
+        }
+    }
+
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
